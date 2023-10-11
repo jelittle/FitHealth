f7843d7dad0d81eafb5ea5ef0ef578f8bbb521eb
BCipi <98548267+BCipi@users.noreply.github.com>
2023-10-10 21:01:08 -0400

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,40 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.ArrayList;
+
+public class Main{
+    public static void main(String[] args){
+        User Bob = new User();
+        Bob.setName("Bobert");
+        Bob.setPassword("Password");
+        System.out.println(Bob.getName());
+        System.out.println(Bob.getPassword());
+        // System.out.println("---------------");
+        // UserInfo.printAll(Bob);
+
+        ArrayList<User> userList = new ArrayList<>();
+        userList.add(Bob);
+        userList.add(new User("Bobby"));
+        System.out.println("---------------");
+        MenuNavigation.startScreen(userList);
+
+        // System.out.println("---------------");
+        // UserInfo.startScreen(userList);
+        // Scanner reader = new Scanner(System.in);
+        // int action = reader.nextInt();
+        // if (action == 4)
+        //     System.out.println("W");
+
+
+        
+        // reader.close();
+
+        // Scanner scany = new Scanner(System.in);
+        // int nums;
+        // String words;
+        // nums = Integer.valueOf(scany.nextLine());
+        // words = scany.nextLine();
+        // System.out.println(nums + words);
     }
-}
+
+
+
+} 
---
+++ src/MenuNavigation.java
@@ -0,0 +1,96 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class MenuNavigation {
+    static Scanner reader = new Scanner(System.in);
+
+    private static void userMenu(User user, ArrayList<User> userList){
+        int action;
+        System.out.println("[1] Name: " + user.getName() +  "\n[2] Password: " + user.getPassword() + "\n[3] Height: " + user.getHeight() + 
+        " m\n[4] Weight: " + user.getWeight() + "kg\n[5] Age: " + user.getAge() + "\n[6] ID: " + user.getID() + "\n[7] Delete Profile" + "\n[8] Logout");
+        action = Integer.valueOf(reader.nextLine());
+        switch(action){
+            case 1: 
+                System.out.println("Enter New Name"); 
+                user.setName(reader.nextLine());   
+                break;
+            case 2: 
+                System.out.println("Enter New Password");
+                user.setPassword(reader.nextLine()); 
+                break;
+            case 3: 
+                System.out.println("Enter New Height");
+                user.setHeight(Double.valueOf(reader.nextLine())); 
+                break;
+            case 4: 
+                System.out.println("Enter New Weight");
+                user.setWeight(Double.valueOf(reader.nextLine())); 
+                break;
+            case 5: 
+                System.out.println("Enter New Age");
+                user.setAge(Integer.valueOf(reader.nextLine())); 
+                break;
+            case 6: 
+                System.out.println("Enter New ID");
+                user.setID(Integer.valueOf(reader.nextLine())); 
+                break;
+            case 7: 
+                System.out.println("Enter Password");
+                if (passwordCheck(user, reader.nextLine())){
+                    userList.remove(user);
+                    startScreen(userList);
+                } else 
+                    userMenu(user, userList);
+            case 8: startScreen(userList); break;
+        }
+        userMenu(user, userList);
+    }
+
+    public static void startScreen(ArrayList<User> userList){
+        int action;
+        int i;
+
+        for (i = 0; i < userList.size(); i++){
+            System.out.println("[" + (i+1) + "] " + userList.get(i).getName());
+        }
+        System.out.println("[" + (i + 1) + "] New User");
+        System.out.println("[" + (i + 2) + "] Shut Down");
+
+        action = Integer.valueOf(reader.nextLine());
+
+        if (action == i + 2){
+            System.exit(0);
+        } else if (action == i + 1){
+            System.out.println("Enter Name");
+            String name = reader.nextLine();
+            User newUser = new User(name);
+            userList.add(newUser);
+            MenuNavigation.userMenu(userList.get(action - 1), userList);
+        } else{
+            
+            if (!userList.get(action - 1).getPassword().equals("")){
+                System.out.println("Enter Password");
+                String password = reader.nextLine();
+                if (passwordCheck(userList.get(action - 1), password))
+                    MenuNavigation.userMenu(userList.get(action - 1), userList);
+                else {
+                    startScreen(userList);
+                }
+            } else
+                MenuNavigation.userMenu(userList.get(action - 1), userList);
+        }
+        
+    }
+
+    public static boolean passwordCheck(User user, String password){
+        if (user.getPassword().equals(password))
+            return true;
+        else {
+            System.out.println("Incorrect password, try again or enter BACK to conacel");
+            password = reader.nextLine();
+            if (!password.equals("BACK"))  
+                passwordCheck(user, password);
+            return false;
+        }
+    }
+}
