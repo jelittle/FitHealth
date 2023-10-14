4f22576a01525c39b7a40ebeedc32219269fb902
jelittle <joshlittle077@gmail.com>
2023-10-14 15:25:42 -0400

    adding database uml to repo and finishing up exerciselog, up until exerciselogs, finishing testdatabase

--- Model/src/userData/ExerciseLogs.java
+++ Model/src/userData/ExerciseLogs.java
@@ -1,8 +1,31 @@
 package userData;
+
+import Database.IExerciseClient;
+import Database.IExerciseClientFactory;
+
 import java.util.ArrayList;
+
+/**
+ * class interfaces with the database to get exercise logs
+ * manages exercise logs for user class
+ * Client class for controller
+ *
+ *
+ *
+ */
 
 class ExerciseLogs {
     private  User user;
     private ArrayList<ExerciseLog> exerciseLogs = new ArrayList<>();
 
+    private static final IExerciseClient db = IExerciseClientFactory.getIExerciseClient();
+
+    ExerciseLogs(User user){
+        this.user = user;
+    }
+    ArrayList<ExerciseLog> GetExerciseLogsByDateRangeAndUserId(long startDate, long EndDate, int userId){
+        exerciseLogs =db.getExerciseLogsByDateRangeAndUserId(startDate, EndDate, userId);
+        return exerciseLogs;
+
+    }
 }
