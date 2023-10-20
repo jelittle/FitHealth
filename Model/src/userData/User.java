d8e152f02ab297bb66ecb0a3328f285471b57e8d
BCipi <98548267+BCipi@users.noreply.github.com>
2023-10-20 02:07:47 -0400

    Add files via upload

--- Model/src/userData/User.java
+++ Model/src/userData/User.java
@@ -3,6 +3,7 @@ package userData;
 public class User{
     private String name;
     private String password;
+    private String sex;
     private int height;
     private float weight;
     private int age; 
@@ -13,6 +14,7 @@ public class User{
     public User(){
         name = "";
         password = "";
+        sex = "";
         height = 0;
         weight = 0;
         age = 0;
@@ -22,6 +24,7 @@ public class User{
     public User(String name){
         this.name = name;
         password = "";
+        sex = "";
         height = 0;
         weight = 0;
         age = 0;
@@ -31,6 +34,17 @@ public class User{
     public User(String name, String password, int height, float weight, int age, int id){
         this.name = name;
         this.password = password;
+        sex = "";
+        this.height = height;
+        this.weight = weight;
+        this.age = age;
+        this.id = id;
+    }
+
+    public User(String name, String password, String sex, int height, float weight, int age, int id){
+        this.name = name;
+        this.password = password;
+        this.sex = sex;
         this.height = height;
         this.weight = weight;
         this.age = age;
@@ -50,6 +64,13 @@ public class User{
     }
     public String getPassword(){
         return password;
+    }
+
+    public void setSex(String sex){
+        this.sex = sex;
+    }
+    public String getSex(){
+        return sex;
     }
 
     public void setHeight(int height){
